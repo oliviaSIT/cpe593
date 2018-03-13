ac93f33c930116aa29d057c28399a5c08bdd6d02
oliviaSIT <olivia.lee8321@gmail.com>
2018-03-13 21:02:16 +0000

    not finsish

--- HW4b.cpp
+++ HW4b.cpp
@@ -1,6 +1,10 @@
 // created by Jiabin.Li
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 
 using namespace std;
  
@@ -94,19 +98,79 @@ public:
 
 		cout << endl;
 	}
+
+        vector<int> getDigit(string s) {
+                int x, y ,z;
+                size_t sz, sz1;
+                x = stoi(s, &sz);
+                string s1 = s.substr(sz + 1);
+                y = stoi(s1, &sz1);
+                string s2 = s1.substr(sz1 + 1);
+                z = stoi(s2, nullptr);
+                vector<int> res;
+                res.push_back(x);
+                res.push_back(y);
+                res.push_back(z);
+                return res;
+        }
+
+
+	void readFile(const char fileName[]) {
+		fstream file;
+		file.open(fileName);
+		string line, instructor, v1;
+		int start, end, step;		
+
+		while (!file.eof()) {
+			getline(file, line);
+			istringstream iss(line);
+			iss >> instructor;
+
+			if (instructor == "ADD_FRONT") {
+				iss >> v1;
+				vector<int> digit = getDigit(v1);
+				start = digit[0];
+				end = digit[2];
+				step = digit[1];
+				for (int i = 0; i <= (end - start) / step; i++) {
+					addFront(start + i * step);
+				}		
+			}
+
+			if (instructor == "ADD_BACK") {
+				iss >> v1;
+				vector<int> digit = getDigit(v1);
+				start = digit[0];
+				end = digit[2];
+				step = digit[1];
+				for (int i = 0; i <= (end - start) / step; i++)
+					addBack(start + i * step);
+				
+			}
+
+			if (instructor == "REMOVE_FRONT") {
+				iss >> step;
+				for (int i = 0; i < step; i++)
+					removeFront();
+			}
+
+			if (instructor == "REMOVE_BACK") {
+				iss >> step;
+				for (int i = 0; i < step; i++)
+					removeBack();
+			}
+
+			if (instructor == "OUTPUT")
+				output();
+		
+		}
+
+	}
+
 };
 
-int main() {
+int main(int argc, char* argv[]) {
 	LinkedList2 list;
-	list.addFront(2);
-	list.output();
-	list.addFront(1);
-	list.output();
-	list.addBack(3);
-	list.output();
-	list.removeFront();
-	list.removeBack();
-	list.output();
-
+	list.readFile(argv[1]);
 	return 0;
 }
